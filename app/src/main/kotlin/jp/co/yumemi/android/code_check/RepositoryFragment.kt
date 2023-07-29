4c1f82d978125e920e45706f04ef4b13f174128b
shigaresso <70837100+shigaresso@users.noreply.github.com>
2023-07-29 23:40:34 +0900

    fragment_two.xml のファイル名を fragment_repository.xml に変更した。
    これに伴って、コードの変更が必要な部分を変更した。
    また、fragment_repository.xml ファイルの Problems の warning に対しては別の Issue を作成したので、そちらで対処する。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoryFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoryFragment.kt
@@ -10,13 +10,13 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import coil.load
 import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
-import jp.co.yumemi.android.code_check.databinding.FragmentTwoBinding
+import jp.co.yumemi.android.code_check.databinding.FragmentRepositoryBinding
 
-class RepositoryFragment : Fragment(R.layout.fragment_two) {
+class RepositoryFragment : Fragment(R.layout.fragment_repository) {
 
     private val args: RepositoryFragmentArgs by navArgs()
 
-    private var binding: FragmentTwoBinding? = null
+    private var binding: FragmentRepositoryBinding? = null
     private val _binding get() = binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -24,7 +24,7 @@ class RepositoryFragment : Fragment(R.layout.fragment_two) {
 
         Log.d("検索した日時", lastSearchDate.toString())
 
-        binding = FragmentTwoBinding.bind(view)
+        binding = FragmentRepositoryBinding.bind(view)
 
         val item = args.item
 
