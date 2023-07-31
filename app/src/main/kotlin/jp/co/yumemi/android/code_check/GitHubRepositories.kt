5488f8b2fa119b5f910c4fb9fac065c3a07b4c5a
shigaresso <70837100+shigaresso@users.noreply.github.com>
2023-07-31 18:23:33 +0900

    WebAPI との通信で受け取る JSON 文字列のデータ構造をクラスで表現した。

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/GitHubRepositories.kt
@@ -0,0 +1,38 @@
+package jp.co.yumemi.android.code_check
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Repositories(
+    @SerialName("incomplete_results")
+    val incompleteResults: Boolean,
+    @SerialName("items")
+    val items: List<RepositoryItem>,
+    @SerialName("total_count")
+    val totalCount: Int
+)
+
+@Serializable
+data class RepositoryItem(
+    @SerialName("full_name")
+    val fullName: String,
+    @SerialName("owner")
+    val owner: Owner,
+    @SerialName("language")
+    val language: String = "",
+    @SerialName("stargazers_count")
+    val stargazersCount: Long,
+    @SerialName("watchers_count")
+    val watchersCount: Long,
+    @SerialName("forks_count")
+    val forksCount: Long,
+    @SerialName("open_issues_count")
+    val openIssuesCount: Long,
+)
+
+@Serializable
+data class Owner(
+    @SerialName("avatar_url")
+    val avatarUrl: String,
+)
