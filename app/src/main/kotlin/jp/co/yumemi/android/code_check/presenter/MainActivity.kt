21ae12048419ef455cf796b0dbfa67e5217e0c5b
shigaresso <70837100+shigaresso@users.noreply.github.com>
2023-08-11 23:24:53 +0900

    アプリ起動時に実行される Activity から Jetpack Compose の UI を呼び出すようにした。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/presenter/MainActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/presenter/MainActivity.kt
@@ -3,13 +3,21 @@
  */
 package jp.co.yumemi.android.code_check.presenter
 
+import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import jp.co.yumemi.android.code_check.R
+import jp.co.yumemi.android.code_check.presenter.ui.screen.SearchRepositoriesScreen
 import java.util.*
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SearchRepositoriesScreen()
+        }
+    }
 
     companion object {
         lateinit var lastSearchDate: Date
