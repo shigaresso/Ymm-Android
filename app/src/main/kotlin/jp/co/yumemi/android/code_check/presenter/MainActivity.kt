83ee4c7e412fdc41f6d44b8d5f52f9866904b1b9
shigaresso <70837100+shigaresso@users.noreply.github.com>
2023-08-15 18:03:38 +0900

    アプリケーションが Navigation Compose を用いた画面遷移へ移行した。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/presenter/MainActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/presenter/MainActivity.kt
@@ -6,7 +6,12 @@ package jp.co.yumemi.android.code_check.presenter
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import jp.co.yumemi.android.code_check.Item
+import jp.co.yumemi.android.code_check.presenter.ui.screen.RepositoryScreen
 import jp.co.yumemi.android.code_check.presenter.ui.screen.SearchRepositoriesScreen
 import java.util.*
 
@@ -15,7 +20,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SearchRepositoriesScreen()
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "searchRepositoriesScreen") {
+                composable("searchRepositoriesScreen") {
+                    SearchRepositoriesScreen() { item ->
+                        navController.currentBackStackEntry?.savedStateHandle?.set(
+                            key = "item",
+                            value = item
+                        )
+                        navController.navigate("repositoryScreen")
+                    }
+                }
+                composable(
+                    "repositoryScreen"
+                ) {
+                    val item =
+                        navController.previousBackStackEntry?.savedStateHandle?.get<Item>("item")
+                    RepositoryScreen(item!!)
+                }
+            }
         }
     }
 
